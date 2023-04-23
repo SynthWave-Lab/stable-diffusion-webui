f89b286eff4e7ed12bd741404f0cf0f4135da717
zhenkaiwang <nighterkai@gmail.com>
2023-04-22 17:34:46 -0700

    Add docker support

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ "$(which docker-compose)" ]; then
+	compose='docker-compose'
+else
+	compose='docker compose'
+fi
+
+$compose up -d
+
+echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker ps --format '{{.Names}} {{.Ports}}' | grep 'webui' | awk '{print $2}' | sed -n -e 's/^0.0.0.0:\([0-9]*\)->.*/\1/p'
+) \n\n"
+echo "To stop showing the logs precc CTRL-C"
+$compose logs -f
